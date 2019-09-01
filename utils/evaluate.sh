fb7a369cf9d433e194129f180375231181fdff80
dfan <davidfan219@gmail.com>
2019-08-31 23:09:49 -0700

    Util folder with Tensorflow evaluation code copied over

---
+++ utils/evaluate.sh
@@ -0,0 +1,14 @@
+BOUNDING_BOXES=/home/dfan/Projects/kaggle/open-images-segmentation-2019/data/challenge-2019-validation-segmentation-bbox
+IMAGE_LABELS=/home/dfan/Projects/kaggle/open-images-segmentation-2019/data/challenge-2019-validation-segmentation-imagelabels
+INPUT_PREDICTIONS=/home/dfan/Projects/kaggle/open-images-segmentation-2019/sample_empty_submission.csv
+INSTANCE_SEGMENTATIONS=/home/dfan/Projects/kaggle/open-images-segmentation-2019/data/challenge-2019-validation-segmentation-bbox
+#INSTANCE_SEGMENTATIONS=/home/dfan/Projects/kaggle/open-images-segmentation-2019/data/masks/validation/challenge-2019-validation-segmentation-masks
+OUTPUT_METRICS=/home/dfan/Projects/kaggle/open-images-segmentation-2019/output/evaluation_output.txt
+
+python oid_challenge_evaluation.py \
+    --input_annotations_boxes=${BOUNDING_BOXES}_expanded.csv \
+    --input_annotations_labels=${IMAGE_LABELS}_expanded.csv \
+    --input_class_labelmap=object_detection/data/oid_object_detection_challenge_500_label_map.pbtxt \
+    --input_predictions=${INPUT_PREDICTIONS} \
+    --input_annotations_segm=${INSTANCE_SEGMENTATIONS}_expanded.csv
+    --output_metrics=${OUTPUT_METRICS} \
---
+++ utils/hierarchical_expansion.sh
@@ -0,0 +1,22 @@
+HIERARCHY_FILE=/home/dfan/Projects/kaggle/open-images-segmentation-2019/data/challenge-2019-label300-segmentable-hierarchy
+BOUNDING_BOXES=/home/dfan/Projects/kaggle/open-images-segmentation-2019/data/challenge-2019-validation-segmentation-bbox
+IMAGE_LABELS=/home/dfan/Projects/kaggle/open-images-segmentation-2019/data/challenge-2019-validation-segmentation-imagelabels
+INSTANCE_SEGMENTATIONS=/home/dfan/Projects/kaggle/open-images-segmentation-2019/data/challenge-2019-validation-segmentation-masks
+
+python oid_hierarchical_labels_expansion.py \
+    --json_hierarchy_file=${HIERARCHY_FILE}.json \
+    --input_annotations=${BOUNDING_BOXES}.csv \
+    --output_annotations=${BOUNDING_BOXES}_expanded.csv \
+    --annotation_type=1
+
+python oid_hierarchical_labels_expansion.py \
+    --json_hierarchy_file=${HIERARCHY_FILE}.json \
+    --input_annotations=${IMAGE_LABELS}.csv \
+    --output_annotations=${IMAGE_LABELS}_expanded.csv \
+    --annotation_type=2
+
+python oid_hierarchical_labels_expansion.py \
+    --json_hierarchy_file=${HIERARCHY_FILE}.json \
+    --input_annotations=${INSTANCE_SEGMENTATIONS}.csv \
+    --output_annotations=${INSTANCE_SEGMENTATIONS}_expanded.csv \
+    --annotation_type=1
