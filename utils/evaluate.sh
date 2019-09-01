ca04564b40f9293f24c561ad25bb66a874fd5dbd
dfan <davidfan219@gmail.com>
2019-09-01 01:14:17 -0700

    Scripts update

--- utils/evaluate.sh
+++ utils/evaluate.sh
@@ -1,11 +1,10 @@
 BOUNDING_BOXES=/home/dfan/Projects/kaggle/open-images-segmentation-2019/data/challenge-2019-validation-segmentation-bbox
 IMAGE_LABELS=/home/dfan/Projects/kaggle/open-images-segmentation-2019/data/challenge-2019-validation-segmentation-imagelabels
-INPUT_PREDICTIONS=/home/dfan/Projects/kaggle/open-images-segmentation-2019/sample_empty_submission.csv
-INSTANCE_SEGMENTATIONS=/home/dfan/Projects/kaggle/open-images-segmentation-2019/data/challenge-2019-validation-segmentation-bbox
-#INSTANCE_SEGMENTATIONS=/home/dfan/Projects/kaggle/open-images-segmentation-2019/data/masks/validation/challenge-2019-validation-segmentation-masks
+INPUT_PREDICTIONS=/home/dfan/Projects/kaggle/open-images-segmentation-2019/sample_validation_submission.csv
+INSTANCE_SEGMENTATIONS=/home/dfan/Projects/kaggle/open-images-segmentation-2019/data/processed-validation-segmentation-masks
 OUTPUT_METRICS=/home/dfan/Projects/kaggle/open-images-segmentation-2019/output/evaluation_output.txt
 
-python oid_challenge_evaluation.py \
+python object_detection/metrics/oid_challenge_evaluation.py \
     --input_annotations_boxes=${BOUNDING_BOXES}_expanded.csv \
     --input_annotations_labels=${IMAGE_LABELS}_expanded.csv \
     --input_class_labelmap=object_detection/data/oid_object_detection_challenge_500_label_map.pbtxt \
